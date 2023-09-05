1126295bebd1f6fc86c955592de9c7e57a5289ad
gian <michaelgiantiqui3@gmail.com>
2023-09-05 22:29:15 +0800

    hehe

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
 import blockchain.Block;
 import blockchain.Blockchain;
+import console.console;
 
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        List<Integer> nums = Arrays.asList(5, 4, 3, 2, 1, 0);
+
+        nums.stream()
+                .filter(n -> n % 2 != 0)
+                .sorted()
+                .map(n -> n*2)
+                .forEach(System.out::println);
+
+        int res = nums.parallelStream()
+                .filter(n -> n % 2 != 0)
+                .map(n -> n*2)
+                .reduce(0, Integer::sum);
+
+        console.log("res = ".concat(String.valueOf(res)));
+    }
+
+    public static void blockchainTest() {
 
         Block genesis = new Block(
                 0,
---
+++ src/console/console.java
@@ -0,0 +1,8 @@
+package console;
+
+public class console {
+
+    public static void log(Object object) {
+        System.out.println(object);
+    }
+}
